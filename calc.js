d24602baf696dd13d9f30ca2947f5d95bbfca293
Sihan <29PathiS@mbbc.qld.edu.au>
2024-04-21 20:00:20 +1000

    updated code

--- calc.js
+++ calc.js
@@ -1,4 +1,6 @@
+const result = document.getElementById('result');
+
 $("button").click(function() {
     var pressedButton = $(this).val();
-    alert(pressedButton);
+    result.value = pressedButton;
 });
