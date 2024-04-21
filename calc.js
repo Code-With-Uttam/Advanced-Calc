1a20e4d3617fdc2b972b8fb9f1aaf0967346b8e2
Sihan <29PathiS@mbbc.qld.edu.au>
2024-04-21 20:17:09 +1000

    a

--- calc.js
+++ calc.js
@@ -2,5 +2,6 @@ const result = document.getElementById('result');
 
 $("button").click(function() {
     var pressedButton = $(this).val();
+    alert(pressedButton);
     result.value = pressedButton;
 });
