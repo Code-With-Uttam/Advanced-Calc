28f049781b6d8f016f6187a83c7ab4f2f4b5cda6
Sihan <29PathiS@mbbc.qld.edu.au>
2024-04-21 20:51:57 +1000

    updated code

--- calc.js
+++ calc.js
@@ -1,12 +1,24 @@
-const result = document.getElementById('result');
+const result1 = document.getElementById('result1');
 
 $("button").click(function() {
     var pressedButton = $(this).val();
+    var firstDigit;
+    var secondDigit;
     if (pressedButton == "=") {
-        result.value = "";
+        secondDigit = result.value;
+        result1.value = "";
     } else if (pressedButton == "C") {
-        result.value = "";
+        result1.value = "";
+    } else if (pressedButton == "+") {
+        firstDigit = result1.value;
+        result1.value += "+";
+        alert(firstDigit);
+        if (pressedButton == "=") {
+            secondDigit = result.value;
+            alert(secondDigit)
+            result1.value = "";
+        }
     } else {
-        result.value = pressedButton;
+        result1.value += pressedButton;
     }
 });
