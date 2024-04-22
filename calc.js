01d69c5682b4d532d873ca67510c2cb41af0c8e3
Sihan <29PathiS@mbbc.qld.edu.au>
2024-04-22 16:09:25 +1000

    Update calc.js

--- calc.js
+++ calc.js
@@ -5,11 +5,16 @@ $("button").click(function() {
     var pressedButton = $(this).val();
     var firstDigit;
     var secondDigit;
+    var firstNumberEntered = 0;
     if (pressedButton == "=") {
         secondDigit = result1.value;
-        result1.value = "";
+        result1.value = "=";
     } else if (pressedButton == "C") {
         result1.value = "";
+    } else if (pressedButton == "+" || pressedButton == "-" || pressedButton == "x" || pressedButton == "÷"){
+        firstDigit = result1.value
+        result1.value = pressedButton;
+        firstNumberEntered = 1;
     } else {
         result1.value += pressedButton;
     }
