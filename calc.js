cb33bcf54ceff5dd1588c8988fafc071ba9db5d4
Sihan <29PathiS@mbbc.qld.edu.au>
2024-04-23 21:37:58 +1000

    Update calc.js

--- calc.js
+++ calc.js
@@ -2,18 +2,29 @@ const result1 = document.getElementById('result1');
 const result2 = document.getElementById('result2');
 let firstDigit;
 let secondDigit;
+let sum;
 
 $("button").click(function() {
     var pressedButton = $(this).val();
     var firstNumberEntered = false;
     if (pressedButton == "=") {
         secondDigit = result1.value;
+        var operation = secondDigit[0];
+        console.log(operation);
         secondDigit = secondDigit.substring(1);
         alert(secondDigit);
         console.log(firstDigit);
         console.log(secondDigit);
         result1.value = "=";
-        let sum = firstDigit + secondDigit;
+        if (operation == "+") {
+            sum = +firstDigit + +secondDigit;
+        } else if (operation == "-") {
+            sum = firstDigit - secondDigit;
+        } else if (operation == "÷") {
+            sum = firstDigit / secondDigit;
+        } else {
+            sum = firstDigit * secondDigit;
+        }
         result2.value = sum;
         console.log(sum);
     } else if (pressedButton == "C") {
